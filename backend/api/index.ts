181cf574849993d27475920c04b555dbf94ac778
kennedy V <kennedycvieira@gmail.com>
2025-03-04 19:17:57 -0300

    fix2

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,10 +1,10 @@
-import { getProducts } from "../models/product";
+//import { getProducts } from "../models/product";
 
 const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
-app.get("/products", (req,res) => res.send(getProducts));
+//app.get("/products", (req,res) => res.send(getProducts));
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
